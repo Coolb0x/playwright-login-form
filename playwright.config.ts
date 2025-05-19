4ee6bc39ad91f70792aaf45e03d5cb7a5fcdc051
Coolb0x <45721224+Coolb0x@users.noreply.github.com>
2025-05-19 09:47:53 +0300

    Update Playwright reporter configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html", { open: "never", outputFolder: "playwright-report" }],
+    ["html", { open: "always", outputFolder: "playwright-report" }],
     ["junit", { outputFile: "test-results/junit-results.xml" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
